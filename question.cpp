3ba25bb0f1a893eeee4030f04e7a4657530230a0
Fares elboukhari <95243744+elboukharif1@users.noreply.github.com>
2022-09-12 11:54:18 +0200

    Add files via upload

---
+++ question.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+int 1;
+int 2;
+int 3;
+int 4;
+
+cout<< "comment va tu ?" <<endl;
+cout<< "1.ca va !"<<endl;
+cout<< "2.coussi coussa !"<<endl;
+cout<< "3.je suis triste!"<<endl;
+cout<< "4.je suis enerver !"<<endl;
+cin<< <<endl;
+return 0;
+}
